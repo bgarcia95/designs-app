47985cb6dd49b1d99b88a08715968515e360a43f
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-12 12:22:04 -0600

    export card tables widget from barrel file

--- lib/src/widgets/widgets.dart
+++ lib/src/widgets/widgets.dart
@@ -1,3 +1,4 @@
 export 'package:layouts/src/widgets/background.dart';
+export 'package:layouts/src/widgets/cards_table.dart';
 export 'package:layouts/src/widgets/custom_bottom_navigation.dart';
 export 'package:layouts/src/widgets/page_title.dart';
